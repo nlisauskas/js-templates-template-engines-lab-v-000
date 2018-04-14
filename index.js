d129e69c882d8e35e0bdaff980fdc50890ba1bf5
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-04-14 20:30:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 function createPost() {
   var title = document.getElementById("postTitle").value;
   var body = document.getElementById("postBody").value;
-  var author = document.getElementById("postAuthor").value;
+  var author =document.getElementById("postAuthor").value;
 
   var pageTemplate = document.getElementById("page-template").innerHTML;
-  var commentsTemplate = document.getElementById("comment-template").innerHTML;
+  var commentsTemplate = document.getElementById("comments-template").innerHTML;
   var postTemplate = document.getElementById("post-template").innerHTML;
 
   var templateFn = _.template(pageTemplate);
 
+//  var start = document.getElementById("comments");
+
   var templateHTML = templateFn();
+//  debugger
 
   document.getElementById("start").innerHTML = templateHTML;
 
   templateFn = _.template(postTemplate);
 
-  templateHTML = templateFn({'title': title, 'body': body, 'author': author})
+  templateHTML = templateFn({ 'title': title, 'body': body,'author': author });
+
+//  debugger
 
   document.getElementById("start").innerHTML += templateHTML;
 
-  templateFn = _.template(commentsTemplate)
+  templateFn = _.template(commentsTemplate);
 
   templateHTML = templateFn();
 
   document.getElementById("start").innerHTML += templateHTML;
-}
 
-function postComment() {
+}
+function postComment(){
   var comment = document.getElementById("commentText").value;
   var commenter = document.getElementById("commenterName").value;
 
@@ -34,7 +39,8 @@ function postComment() {
 
   var templateFn = _.template(commentTemplate);
 
-  var templateHTML = templateFn({'comment': comment, 'commenter': commenter})
+  var templateHTML = templateFn({'comment':comment, 'commenter':commenter});
 
-  document.getElementByID("comments").innerHTML += templateHTML;
+  document.getElementById("comments").innerHTML += templateHTML;
+
 }
