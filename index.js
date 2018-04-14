a4c9102100f3560634f3b9a59b06e9497563ccab
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-04-14 20:12:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ function createPost() {
   var title = document.getElementById("postTitle").value;
   var body = document.getElementById("postBody").value;
   var author = document.getElementById("postAuthor").value;
+
+  var pageTemplate = document.getElementById("page-template").innerHTML;
+  var commentsTemplate = document.getElementById("comment-template").innerHTML;
 }
 
 function postComment() {
